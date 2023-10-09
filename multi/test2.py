33651fb110c62545eeff0b52d6a5ceef055a1bd4
KopekC <k0p3k@riseup.net>
2023-10-09 19:41:04 -0400

    artificial conflict

---
+++ multi/test2.py
@@ -0,0 +1,14 @@
+def function_one():
+    # Some comment about function_one
+    print("This is function one from HEAD")
+
+def function_two():
+    print("Function two implementation from HEAD")
+
+# Some other non-conflicting lines
+x = 10
+y = 20
+
+def function_three():
+    # A comment about function three
+    print("Function three implementation from HEAD")
